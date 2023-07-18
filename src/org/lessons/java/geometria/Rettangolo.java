9b658260b61cc054a099086476d247fb43f4f9d6
FrancescoGuggino <landongu@gmail.com>
2023-07-18 14:16:12 +0200

    metodi

--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -3,7 +3,17 @@ package org.lessons.java.geometria;
 public class Rettangolo {
 
     // CAMPI
+    int base;
+    int altezza;
+
     // COSTRUTTORI
+
+    Rettangolo(int base, int altezza){
+        this.altezza = altezza;
+        this.base = base;
+    }
     // METODI
+
+
 
 }
